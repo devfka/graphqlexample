becd01ee28e45c5e07222a6a35ee7cc148c7f286
Fatih Kemal Akcay <fatihkemalakcay@gmail.com>
2020-04-19 23:20:10 +0100

    first example

---
+++ src/main/java/com/graphqlexample/graphql/FoodRepository.java
@@ -0,0 +1,8 @@
+package com.graphqlexample.graphql;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface FoodRepository extends JpaRepository<Food, Long> {
+}
